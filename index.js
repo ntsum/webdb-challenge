a58d0c317a2c0373e88adb6aaedcc2ff43ca3c11
ntsum <natsukajiwaram@gmail.com>
2019-05-03 11:35:05 -0400

    added in new folders for migrations and for action/project router

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ server.use(express.json());
 const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
-    console.log(`Server is live on ${port}`);
+    console.log(`Server is live on ${port}`)
 })
